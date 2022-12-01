3d959528264c12f91503fa925c27d6d4beefcba6
ROCA <benny13350@gmail.com>
2022-12-01 15:20:04 +0100

    First commit of the app under java.swing libraries, to rectify soon

--- fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
+++ fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
@@ -1,9 +1,21 @@
 package fr.tse.prinfo3;
 
+import fr.tse.prinfo3.control.Controller;
+import fr.tse.prinfo3.model.Model;
+import fr.tse.prinfo3.view.ViewMenu;
+// import fr.tse.prinfo3.view.TemperatureVueCelsius;
 public class main {
-
-	public static void main(String[] args) {
-		System.out.println("hello project info3");
-	}
-
+    public main() {
+        Model tempmod = new Model();
+        Controller tempcontrolMenu = new Controller(tempmod);
+        //Controller tempcontrolF = new Controller(tempmod);
+        ViewMenu pvc = new ViewMenu(tempmod, tempcontrolMenu, 100, 100);
+        //TemperatureVueFarenheit tvf = new TemperatureVueFarenheit(tempmod, tempcontrolF, 100, 350);
+        tempcontrolMenu.setView(pvc);
+        //tempcontrolF.setView(tvf);
+    }
+    
+    public static void main(String args []) {
+        new main();
+    }
 }
