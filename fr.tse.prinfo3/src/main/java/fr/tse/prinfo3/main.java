4a222217a9dc8b221688ea622ad4bf1e7e032a42
lardon.adrian <adrian.lardon@telecom-st-etienne.fr>
2022-12-03 14:19:31 +0100

    Modification mineur

--- fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
+++ fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
@@ -1,10 +1,5 @@
 package fr.tse.prinfo3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class main {
 
