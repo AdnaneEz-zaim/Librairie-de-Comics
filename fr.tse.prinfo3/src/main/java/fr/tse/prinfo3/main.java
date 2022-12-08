450f35ce2e7f6ee9c449c737e786affa2fb6fd26
lardon.adrian <adrian.lardon@telecom-st-etienne.fr>
2022-12-08 11:24:09 +0100

    Modification arborescence et load fichier FXML

--- fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
+++ fr.tse.prinfo3/src/main/java/fr/tse/prinfo3/main.java
@@ -1,20 +1,9 @@
 package fr.tse.prinfo3;
-import fr.tse.prinfo3.control.Controller;
-import fr.tse.prinfo3.model.Model;
-import fr.tse.prinfo3.view.ViewMenu;
+import fr.tse.prinfo3.control.DatabaseOperations;
 
-// import fr.tse.prinfo3.view.TemperatureVueCelsius;
+import fr.tse.prinfo3.view.openMenuFXML;
+
 public class main {
-
-	public main() {
-		Model tempmod = new Model();
-		Controller tempcontrolMenu = new Controller(tempmod);
-		//Controller tempcontrolF = new Controller(tempmod);
-		ViewMenu pvc = new ViewMenu(tempmod, tempcontrolMenu, 100, 100);
-		//TemperatureVueFarenheit tvf = new TemperatureVueFarenheit(tempmod, tempcontrolF, 100, 350);
-		tempcontrolMenu.setView(pvc);
-		//tempcontrolF.setView(tvf);
-	    }
 
 	public static void main(String[] args) {
 		System.out.println("hello project info3");
@@ -24,11 +13,11 @@ public class main {
 		String username = "root";
 		String password = "";
 		DatabaseOperations dbComic = new DatabaseOperations(hostname, db, port, username, password);
-		dbComic.selectAllUser();
+		//dbComic.selectAllUser();
 		//dbComic.insertUser("lardon.adrian@gmail.com", "1234", "Adrian");
 		//dbComic.selectAllUser();
 		dbComic.close();
-		new main();
+		openMenuFXML.main(args);
 	}
 
 }
